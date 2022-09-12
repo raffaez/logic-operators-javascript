3fbc0b5ee1723233b80b5d8f83948ba5925aa70a
raffaez <rafaeleasouzas@gmail.com>
2022-09-12 12:41:29 -0300

    6 fix

--- script6.js
+++ script6.js
@@ -13,4 +13,4 @@ const n1 = Math.pow(x2-x1, 2);
 const n2 = Math.pow(y2-y1, 2);
 const d = Math.sqrt(n1+n2);
 
-console.log(`√(${x2}-${x1})²+(${y2}-${y1})² = ${d.toFixed(1)}`);
+console.log(`The distance between p1(${x1},${y1}) and p2(${x2},${y2}) is ${Math.round(d)}`);
